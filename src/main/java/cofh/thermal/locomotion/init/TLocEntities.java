40215bfa77ca32382937fe62b2f535c445c136e5
King Lemming <kinglemming@gmail.com>
2022-04-17 22:19:11 -0700

    Cleanups, update version.

--- src/main/java/cofh/thermal/locomotion/init/TLocEntities.java
+++ src/main/java/cofh/thermal/locomotion/init/TLocEntities.java
@@ -1,11 +1,15 @@
 package cofh.thermal.locomotion.init;
 
+import cofh.thermal.core.entity.explosive.DetonateUtils;
 import cofh.thermal.locomotion.entity.UnderwaterMinecartEntity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 
 import static cofh.thermal.core.ThermalCore.ENTITIES;
-import static cofh.thermal.locomotion.init.TLocIDs.ID_UNDERWATER_CART;
+import static cofh.thermal.core.util.RegistrationHelper.registerTNTMinecart;
+import static cofh.thermal.lib.common.ThermalFlags.*;
+import static cofh.thermal.lib.common.ThermalIDs.*;
+import static cofh.thermal.locomotion.init.TLocIDs.*;
 
 public class TLocEntities {
 
@@ -17,19 +21,18 @@ public class TLocEntities {
 
         ENTITIES.register(ID_UNDERWATER_CART, () -> EntityType.Builder.<UnderwaterMinecartEntity>of(UnderwaterMinecartEntity::new, EntityClassification.MISC).sized(0.98F, 0.7F).build(ID_UNDERWATER_CART));
 
-        //ENTITIES.register(ID_SLIME_TNT_CART, () -> EntityType.Builder.<SlimeTNTMinecartEntity>of(SlimeTNTMinecartEntity::new, EntityClassification.MISC).sized(0.98F, 0.7F).build(ID_SLIME_TNT_CART));
-        //ENTITIES.register(ID_REDSTONE_TNT_CART, () -> EntityType.Builder.<RedstoneTNTMinecartEntity>of(RedstoneTNTMinecartEntity::new, EntityClassification.MISC).sized(0.98F, 0.7F).build(ID_REDSTONE_TNT_CART));
-        //ENTITIES.register(ID_GLOWSTONE_TNT_CART, () -> EntityType.Builder.<GlowstoneTNTMinecartEntity>of(GlowstoneTNTMinecartEntity::new, EntityClassification.MISC).sized(0.98F, 0.7F).build(ID_GLOWSTONE_TNT_CART));
-        //ENTITIES.register(ID_ENDER_TNT_CART, () -> EntityType.Builder.<EnderTNTMinecartEntity>of(EnderTNTMinecartEntity::new, EntityClassification.MISC).sized(0.98F, 0.7F).build(ID_ENDER_TNT_CART));
-        //
-        //ENTITIES.register(ID_PHYTO_TNT_CART, () -> EntityType.Builder.<PhytoTNTMinecartEntity>of(PhytoTNTMinecartEntity::new, EntityClassification.MISC).sized(0.98F, 0.7F).build(ID_PHYTO_TNT_CART));
-        //
-        //ENTITIES.register(ID_FIRE_TNT_CART, () -> EntityType.Builder.<FireTNTMinecartEntity>of(FireTNTMinecartEntity::new, EntityClassification.MISC).sized(0.98F, 0.7F).build(ID_FIRE_TNT_CART));
-        //ENTITIES.register(ID_EARTH_TNT_CART, () -> EntityType.Builder.<EarthTNTMinecartEntity>of(EarthTNTMinecartEntity::new, EntityClassification.MISC).sized(0.98F, 0.7F).build(ID_EARTH_TNT_CART));
-        //ENTITIES.register(ID_ICE_TNT_CART, () -> EntityType.Builder.<IceTNTMinecartEntity>of(IceTNTMinecartEntity::new, EntityClassification.MISC).sized(0.98F, 0.7F).build(ID_ICE_TNT_CART));
-        //ENTITIES.register(ID_LIGHTNING_TNT_CART, () -> EntityType.Builder.<LightningTNTMinecartEntity>of(LightningTNTMinecartEntity::new, EntityClassification.MISC).sized(0.98F, 0.7F).build(ID_LIGHTNING_TNT_CART));
-        //
-        //ENTITIES.register(ID_NUKE_TNT_CART, () -> EntityType.Builder.<NukeTNTMinecartEntity>of(NukeTNTMinecartEntity::new, EntityClassification.MISC).sized(0.98F, 0.7F).build(ID_NUKE_TNT_CART));
+        registerTNTMinecart(ID_FIRE_TNT_CART, ID_FIRE_TNT, DetonateUtils::fire, getFlag(FLAG_ELEMENTAL_EXPLOSIVES));
+        registerTNTMinecart(ID_ICE_TNT_CART, ID_ICE_TNT, DetonateUtils::ice, getFlag(FLAG_ELEMENTAL_EXPLOSIVES));
+        registerTNTMinecart(ID_LIGHTNING_TNT_CART, ID_LIGHTNING_TNT, DetonateUtils::lightning, getFlag(FLAG_ELEMENTAL_EXPLOSIVES));
+        registerTNTMinecart(ID_EARTH_TNT_CART, ID_EARTH_TNT, DetonateUtils::earth, getFlag(FLAG_ELEMENTAL_EXPLOSIVES));
+
+        registerTNTMinecart(ID_ENDER_TNT_CART, ID_ENDER_TNT, DetonateUtils::ender, getFlag(FLAG_BASIC_EXPLOSIVES));
+        registerTNTMinecart(ID_GLOWSTONE_TNT_CART, ID_GLOWSTONE_TNT, DetonateUtils::glow, getFlag(FLAG_BASIC_EXPLOSIVES));
+        registerTNTMinecart(ID_REDSTONE_TNT_CART, ID_REDSTONE_TNT, DetonateUtils::redstone, getFlag(FLAG_BASIC_EXPLOSIVES));
+        registerTNTMinecart(ID_SLIME_TNT_CART, ID_SLIME_TNT, DetonateUtils::slime, getFlag(FLAG_BASIC_EXPLOSIVES));
+
+        registerTNTMinecart(ID_PHYTO_TNT_CART, ID_PHYTO_TNT, DetonateUtils::phyto, getFlag(FLAG_PHYTOGRO_EXPLOSIVES));
+        registerTNTMinecart(ID_NUKE_TNT_CART, ID_NUKE_TNT, DetonateUtils::nuke, getFlag(FLAG_NUCLEAR_EXPLOSIVES));
     }
 
 }
