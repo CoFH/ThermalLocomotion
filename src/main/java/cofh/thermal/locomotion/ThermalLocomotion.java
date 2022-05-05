da7333f87e7f34902b80a92f5a8f53121a4ac4d7
King Lemming <kinglemming@gmail.com>
2022-05-05 16:07:29 -0700

    Underwater Minecart!

--- src/main/java/cofh/thermal/locomotion/ThermalLocomotion.java
+++ src/main/java/cofh/thermal/locomotion/ThermalLocomotion.java
@@ -1,10 +1,13 @@
 package cofh.thermal.locomotion;
 
+import cofh.thermal.locomotion.client.renderer.entity.UnderwaterMinecartRenderer;
+import cofh.thermal.locomotion.client.renderer.entity.model.UnderwaterMinecartModel;
 import cofh.thermal.locomotion.init.TLocBlocks;
 import cofh.thermal.locomotion.init.TLocEntities;
 import cofh.thermal.locomotion.init.TLocItems;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -14,6 +17,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import static cofh.lib.util.constants.Constants.ID_THERMAL_LOCOMOTION;
 import static cofh.thermal.core.ThermalCore.BLOCKS;
 import static cofh.thermal.locomotion.init.TLocIDs.*;
+import static cofh.thermal.locomotion.init.TLocReferences.UNDERWATER_CART_ENTITY;
 
 @Mod (ID_THERMAL_LOCOMOTION)
 public class ThermalLocomotion {
@@ -24,6 +28,8 @@ public class ThermalLocomotion {
 
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        modEventBus.addListener(this::entityLayerSetup);
+        modEventBus.addListener(this::entityRendererSetup);
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::clientSetup);
 
@@ -38,6 +44,16 @@ public class ThermalLocomotion {
     }
 
     // region INITIALIZATION
+    private void entityLayerSetup(final EntityRenderersEvent.RegisterLayerDefinitions event) {
+
+        event.registerLayerDefinition(UnderwaterMinecartModel.UNDERWATER_MINECART_LAYER, UnderwaterMinecartModel::createMesh);
+    }
+
+    private void entityRendererSetup(final EntityRenderersEvent.RegisterRenderers event) {
+
+        event.registerEntityRenderer(UNDERWATER_CART_ENTITY, UnderwaterMinecartRenderer::new);
+    }
+
     private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
@@ -45,7 +61,6 @@ public class ThermalLocomotion {
     private void clientSetup(final FMLClientSetupEvent event) {
 
         this.registerRenderLayers();
-        this.registerEntityRenderingHandlers();
     }
     // endregion
 
@@ -67,11 +82,6 @@ public class ThermalLocomotion {
         ItemBlockRenderTypes.setRenderLayer(BLOCKS.get(ID_LUMIUM_ACTIVATOR_RAIL), cutout);
         ItemBlockRenderTypes.setRenderLayer(BLOCKS.get(ID_LUMIUM_DETECTOR_RAIL), cutout);
         ItemBlockRenderTypes.setRenderLayer(BLOCKS.get(ID_LUMIUM_POWERED_RAIL), cutout);
-    }
-
-    private void registerEntityRenderingHandlers() {
-
-        // RenderingRegistry.registerEntityRenderingHandler(UNDERWATER_CART_ENTITY, UnderwaterMinecartRenderer::new);
     }
     // endregion
 }
--- src/main/java/cofh/thermal/locomotion/client/renderer/entity/model/UnderwaterMinecartModel.java
+++ src/main/java/cofh/thermal/locomotion/client/renderer/entity/model/UnderwaterMinecartModel.java
@@ -1,42 +1,53 @@
-//package cofh.thermal.locomotion.client.renderer.entity.model;
-//
-//import com.mojang.blaze3d.matrix.MatrixStack;
-//import com.mojang.blaze3d.vertex.IVertexBuilder;
-//import net.minecraft.client.renderer.entity.model.EntityModel;
-//import net.minecraft.client.renderer.model.ModelRenderer;
-//import net.minecraft.entity.Entity;
-//
-//public class UnderwaterMinecartModel<T extends Entity> extends EntityModel<T> {
-//
-//    private final ModelRenderer dome;
-//    private final ModelRenderer minecart;
-//
-//    public UnderwaterMinecartModel() {
-//
-//        texWidth = 128;
-//        texHeight = 128;
-//
-//        dome = new ModelRenderer(this);
-//        dome.addBox(-8, -20, -10, 16, 15, 20);
-//
-//        minecart = new ModelRenderer(this);
-//        minecart.texOffs(0, 42).addBox(-8, -5, -10, 16, 10, 20);
-//        minecart.texOffs(56, 56).addBox(-6, -5, -8, 12, 8, 16);
-//
-//        dome.yRot = ((float) Math.PI / 2F);
-//        minecart.yRot = ((float) Math.PI / 2F);
-//    }
-//
-//    @Override
-//    public void setupAnim(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-//        //previously the render function, render code was moved to a method below
-//    }
-//
-//    @Override
-//    public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-//
-//        dome.render(matrixStack, buffer, packedLight, packedOverlay);
-//        minecart.render(matrixStack, buffer, packedLight, packedOverlay);
-//    }
-//
-//}
+package cofh.thermal.locomotion.client.renderer.entity.model;
+
+import net.minecraft.client.model.HierarchicalModel;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+
+public class UnderwaterMinecartModel<T extends Entity> extends HierarchicalModel<T> {
+
+    public static final ModelLayerLocation UNDERWATER_MINECART_LAYER = new ModelLayerLocation(new ResourceLocation("thermal:underwater_minecart"), "main");
+
+    private final ModelPart root;
+
+    public UnderwaterMinecartModel(ModelPart root) {
+
+        this.root = root;
+    }
+
+    public static LayerDefinition createMesh() {
+
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+
+        partdefinition.addOrReplaceChild("cart", CubeListBuilder.create()
+                        .texOffs(0, 42).addBox(-8, -5, -10, 16, 10, 20)
+                        .texOffs(56, 56).addBox(-6, -5, -8, 12, 8, 16),
+                PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, (float) (Math.PI / 2F), 0.0F));
+
+        partdefinition.addOrReplaceChild("dome", CubeListBuilder.create()
+                        .texOffs(0, 0).addBox(-8, -20, -10, 16, 15, 20),
+                PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, (float) (Math.PI / 2F), 0.0F));
+
+        return LayerDefinition.create(meshdefinition, 128, 128);
+    }
+
+    @Override
+    public ModelPart root() {
+
+        return this.root;
+    }
+
+    @Override
+    public void setupAnim(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
+    }
+
+}
