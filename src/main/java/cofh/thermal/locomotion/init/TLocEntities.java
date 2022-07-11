b8a6c4eeb603ad4873e7fd5774c01d371e75152b
King Lemming <kinglemming@gmail.com>
2022-07-10 23:25:12 -0700

    Update build.gradle

--- src/main/java/cofh/thermal/locomotion/init/TLocEntities.java
+++ src/main/java/cofh/thermal/locomotion/init/TLocEntities.java
@@ -1,12 +1,16 @@
 package cofh.thermal.locomotion.init;
 
+import cofh.thermal.core.entity.explosive.DetonateUtils;
 import cofh.thermal.locomotion.entity.UnderwaterMinecart;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.registries.RegistryObject;
 
 import static cofh.thermal.core.ThermalCore.ENTITIES;
-import static cofh.thermal.locomotion.init.TLocIDs.ID_UNDERWATER_CART;
+import static cofh.thermal.core.util.RegistrationHelper.registerTNTMinecart;
+import static cofh.thermal.lib.common.ThermalFlags.*;
+import static cofh.thermal.lib.common.ThermalIDs.*;
+import static cofh.thermal.locomotion.init.TLocIDs.*;
 
 public class TLocEntities {
 
@@ -19,18 +23,18 @@ public class TLocEntities {
         // ENTITIES.register(ID_ENERGY_CART, () -> EntityType.Builder.<EnergyMinecart>of(EnergyMinecart::new, MobCategory.MISC).sized(0.98F, 0.7F).build(ID_ENERGY_CART));
         // ENTITIES.register(ID_FLUID_CART, () -> EntityType.Builder.<FluidMinecart>of(FluidMinecart::new, MobCategory.MISC).sized(0.98F, 0.7F).build(ID_FLUID_CART));
 
-        //        registerTNTMinecart(ID_FIRE_TNT_CART, ID_FIRE_TNT, DetonateUtils::fire, getFlag(FLAG_ELEMENTAL_EXPLOSIVES));
-        //        registerTNTMinecart(ID_ICE_TNT_CART, ID_ICE_TNT, DetonateUtils::ice, getFlag(FLAG_ELEMENTAL_EXPLOSIVES));
-        //        registerTNTMinecart(ID_LIGHTNING_TNT_CART, ID_LIGHTNING_TNT, DetonateUtils::lightning, getFlag(FLAG_ELEMENTAL_EXPLOSIVES));
-        //        registerTNTMinecart(ID_EARTH_TNT_CART, ID_EARTH_TNT, DetonateUtils::earth, getFlag(FLAG_ELEMENTAL_EXPLOSIVES));
-        //
-        //        registerTNTMinecart(ID_ENDER_TNT_CART, ID_ENDER_TNT, DetonateUtils::ender, getFlag(FLAG_BASIC_EXPLOSIVES));
-        //        registerTNTMinecart(ID_GLOWSTONE_TNT_CART, ID_GLOWSTONE_TNT, DetonateUtils::glow, getFlag(FLAG_BASIC_EXPLOSIVES));
-        //        registerTNTMinecart(ID_REDSTONE_TNT_CART, ID_REDSTONE_TNT, DetonateUtils::redstone, getFlag(FLAG_BASIC_EXPLOSIVES));
-        //        registerTNTMinecart(ID_SLIME_TNT_CART, ID_SLIME_TNT, DetonateUtils::slime, getFlag(FLAG_BASIC_EXPLOSIVES));
-        //
-        //        registerTNTMinecart(ID_PHYTO_TNT_CART, ID_PHYTO_TNT, DetonateUtils::phyto, getFlag(FLAG_PHYTOGRO_EXPLOSIVES));
-        //        registerTNTMinecart(ID_NUKE_TNT_CART, ID_NUKE_TNT, DetonateUtils::nuke, getFlag(FLAG_NUCLEAR_EXPLOSIVES));
+        registerTNTMinecart(ID_FIRE_TNT_CART, ID_FIRE_TNT, DetonateUtils::fire, getFlag(FLAG_ELEMENTAL_EXPLOSIVES));
+        registerTNTMinecart(ID_ICE_TNT_CART, ID_ICE_TNT, DetonateUtils::ice, getFlag(FLAG_ELEMENTAL_EXPLOSIVES));
+        registerTNTMinecart(ID_LIGHTNING_TNT_CART, ID_LIGHTNING_TNT, DetonateUtils::lightning, getFlag(FLAG_ELEMENTAL_EXPLOSIVES));
+        registerTNTMinecart(ID_EARTH_TNT_CART, ID_EARTH_TNT, DetonateUtils::earth, getFlag(FLAG_ELEMENTAL_EXPLOSIVES));
+
+        registerTNTMinecart(ID_ENDER_TNT_CART, ID_ENDER_TNT, DetonateUtils::ender, getFlag(FLAG_BASIC_EXPLOSIVES));
+        registerTNTMinecart(ID_GLOWSTONE_TNT_CART, ID_GLOWSTONE_TNT, DetonateUtils::glow, getFlag(FLAG_BASIC_EXPLOSIVES));
+        registerTNTMinecart(ID_REDSTONE_TNT_CART, ID_REDSTONE_TNT, DetonateUtils::redstone, getFlag(FLAG_BASIC_EXPLOSIVES));
+        registerTNTMinecart(ID_SLIME_TNT_CART, ID_SLIME_TNT, DetonateUtils::slime, getFlag(FLAG_BASIC_EXPLOSIVES));
+
+        registerTNTMinecart(ID_PHYTO_TNT_CART, ID_PHYTO_TNT, DetonateUtils::phyto, getFlag(FLAG_PHYTOGRO_EXPLOSIVES));
+        registerTNTMinecart(ID_NUKE_TNT_CART, ID_NUKE_TNT, DetonateUtils::nuke, getFlag(FLAG_NUCLEAR_EXPLOSIVES));
     }
 
     public static final RegistryObject<EntityType<UnderwaterMinecart>> UNDERWATER_CART = ENTITIES.register(ID_UNDERWATER_CART, () -> EntityType.Builder.<UnderwaterMinecart>of(UnderwaterMinecart::new, MobCategory.MISC).sized(0.98F, 0.7F).build(ID_UNDERWATER_CART));
